e6243a0836474ca05127640f32cf4358817057ef
4elovel <109417314+4elovel@users.noreply.github.com>
2023-10-13 18:24:26 +0300

    Add files via upload

---
+++ counts_main/Program.cs
@@ -0,0 +1,18 @@
+﻿using counts;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Count cnt = new Count(1, 1, 1, 1, 1, "Note");
+        cnt.print();
+        Console.WriteLine("\n");
+        cnt.balance += 1000;
+        cnt.print();
+        Console.WriteLine("\n");
+        Count cnt2 = new Count(25, 21, 23, 12, 1442, "Note2");
+        cnt2.print();
+    }
+}
